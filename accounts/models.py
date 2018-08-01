0d7c112d61920e4a3eff03773606d6eb9530ff25
Normaltic1018 <rlagkstn1426@naver.com>
2018-08-01 23:02:11 +0900

    2018-08-01; extend user;

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    class Meta:
+        verbose_name= 'Profile'
+        verbose_name_plural = 'Profile'
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nick = models.CharField(verbose_name='NickName', max_length=50, blank=True,)
