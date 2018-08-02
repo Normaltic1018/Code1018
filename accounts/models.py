442460d64a073b1cbe6bfc76e8b57d0fe21896e8
Normaltic1018 <rlagkstn1426@naver.com>
2018-08-02 22:32:12 +0900

    2018-08-02; Extend User Complete;

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
     class Meta:
         verbose_name= 'Profile'
         verbose_name_plural = 'Profile'
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     nick = models.CharField(verbose_name='NickName', max_length=50, blank=True,)
     birth_date = models.DateTimeField(null=True, blank=True)
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import redirect
 from .forms import ProfileForm, UserCreationMultiForm
 
 from django.contrib.auth.models import User
@@ -15,3 +16,10 @@ class UserSignupView(generic.CreateView):
     form_class = UserCreationMultiForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+    def form_valid(self, form):
+        user = form['user'].save()
+        profile = form['profile'].save(commit=False)
+        profile.user = user
+        profile.save()
+        return redirect(self.success_url)
