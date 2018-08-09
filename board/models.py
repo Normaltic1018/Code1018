034ba09b48e990c433c709818e735372ee563ec0
Normaltic1018 <rlagkstn1426@naver.com>
2018-08-09 22:33:32 +0900

    2018-08-09; on working post models

--- board/models.py
+++ board/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    writer = models.CharField(max_length=100)
+    post_date = models.DateTimeField(auto_now_add=True)
+    post_title = models.CharField(max_length=100)
+    post_contents = models.TextField(blank=True,help_text='Post Contents')
+
+    class Meta:
+        ordering = ['post_date']
+
+    def __str__(self):
+        return post_title
+
+class Comment(models.Model):
+    comment_date = models.DateTimeField(auto_now_add=True)
+    comment_contents = models.CharField(max_length=200)
+    comment_write = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    writer_nick = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['comment_date']
--- board/views.py
+++ board/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Post, Comment
+from django.views import generic
 
 # Create your views here.
+class AllPostListView(generic.ListView):
+    model = Post
+
 def board_index(request):
+
     return render(request, 'board_index.html')
+
+class AllLoanedBooksListView(PermissionRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    permission_required = 'catalog.can_mark_returned'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
